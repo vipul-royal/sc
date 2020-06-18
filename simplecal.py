3e7c1da1e952954e7056a0e314e52a951f6f257a
vipul <66994343+vipul-royal@users.noreply.github.com>
2020-06-18 14:34:15 +0530

    Add files via upload

---
+++ simplecal.py
@@ -0,0 +1,30 @@
+x=int(input ('enter the values :\t'))
+y=int(input('enter the values:\t'))
+
+# addition
+print('addition of given two numbers are:	')
+print(x+y)
+
+#sub
+print('subtraction of given two numbers are:	')
+print(x-y)
+
+#mul
+print('multiplication of given two numbers are:	')
+print(x*y)
+
+#div
+print('division of given two numbers are:	')
+print(x/y)
+
+#mod
+print('modulas of given two numbers are:	')
+print(x%y)
+
+#expo
+print('exponent of given two numbers are:	')
+print(x**y)
+ 
+#floordiv
+print('floor division of given two numbers are:	')
+print(x//y)
